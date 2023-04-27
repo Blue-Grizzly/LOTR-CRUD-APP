41c59388efcee843e675863f3e97cb94cd5f8b85
Blue-Grizzly <123905990+Blue-Grizzly@users.noreply.github.com>
2023-04-27 11:33:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 "use strict"
 
-const endpoint = "https://lotr-crud-default-rtdb.europe-west1.firebasedatabase.app/"
+const endpoint = "https://lotr-crud-default-rtdb.europe-west1.firebasedatabase.app/";
