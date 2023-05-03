532d25d0f883ae61372168762cf7be889ab7bfeb
Blue-Grizzly <123905990+Blue-Grizzly@users.noreply.github.com>
2023-05-03 08:43:39 +0200

    moved helpers into helpers file

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import { getCharacters, createCharacter, updateCharacter, deleteCharacter } from "./rest-service.js";
-
+import { filterByRace, sortByOption, searchByName } from "./helpers.js";
 
 let characterList;
 
@@ -220,7 +220,6 @@ async function updateCharactersGrid() {
 
 function showCharacters(characterList) {
   document.querySelector("#characters").innerHTML = "";
-  console.log(characterList);
 
   for (const character of characterList) {
     showCharacter(character);
@@ -291,33 +290,4 @@ async function deleteCharacterClicked(characterObject) {
     }
 }
 
-function searchByName(searchValue) {
-  searchValue = searchValue.toLowerCase().trim();
-  return characterList.filter(checkNames);
-
-  function checkNames(character) {
-    return character.name.toLowerCase().includes(searchValue);
-  }
-}
-
-function sortByOption(sortValue) {
-  if (sortValue === "name") {
-    return characterList.sort((a, b) => a.name.localeCompare(b.name));
-  } else if (sortValue === "age") {
-    return characterList.sort((a, b) => a.age - b.age);
-  } else if (sortValue === "title") {
-    return characterList.sort((a, b) => a.title.localeCompare(b.title));
-  } else if (sortValue === "race") {
-    return characterList.sort((a, b) => a.race.localeCompare(b.race));
-  }
-}
-
-function filterByRace(inputValue) {
-  inputValue = inputValue.toLowerCase();
-  if(inputValue !== "filterall"){
-  return characterList.filter((character) =>
-    character.race.toLowerCase().includes(inputValue)
-  );} else{
-    return characterList;
-  }
-}
+export {characterList};
